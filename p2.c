89815d0642df7ba1faa49986c88ca22a56f48314
syedhazeera <129369062+syedhazeera@users.noreply.github.com>
2023-03-31 14:15:35 +0530

    Add files via upload

---
+++ p2.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main( )
+{
+int a,b; 
+printf("Enter values for a and b: ");
+scanf("%d %d",&a,&b);
+printf ("a = %d\t ",a); //bit_pattern(a);
+printf ("b = %d \t",b); //bit_pattern (b);
+printf("a|b = %d\t\t",a|b);// bit_pattern(a&b);
+return 0;
+}
