582a0b5bbfebac0ce0280ef8e3824480056944f7
syedhazeera <129369062+syedhazeera@users.noreply.github.com>
2023-04-05 13:10:01 +0530

    Add files via upload

---
+++ stacks and queues/stack.c
@@ -0,0 +1,64 @@
+#include<stdio.h>
+#include<stdlib.h>
+#define stack_size 10
+int s[stack_size];
+int top = -1;
+void push(int item)
+{
+	if(top == stack_size -1)
+	printf("stack overflow\n");
+	top = top+1;
+	s[top] = item;
+}
+int pop()
+{
+	int item_deleted;
+
+	if(top==-1)
+	{
+		printf("stack is empty\n");
+	return 0;
+	}
+	item_deleted = s[top--];
+//
+	return item_deleted;
+}
+void display()
+{
+	int i;
+	if(top==-1)
+	{
+	printf("stack is empty");
+	}
+	printf("contents of the stack\n");
+	for(i=0;i<=top;i++)
+	printf("%d\n",s[i]);
+}
+int main()
+{
+	int choice,item,item_deleted;
+	for(;;)
+	{
+		printf("enter your choice of operation\n1.push\n2.pop\n3.display\n");
+		scanf("%d",&choice);
+	switch(choice)
+	{
+	case 1:
+		printf("enter the item to be inserted \n");
+		scanf("%d",&item);
+		push(item);
+		break;
+	case 2:
+		printf("poped item is%d\n",pop());
+
+		break;
+	case 3:
+		display();
+		break;
+	default:
+		printf("enter a valid function\n");
+	}
+	}
+}
+		
+
