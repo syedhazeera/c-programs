960230a66ca80d0c0929c6316fe6ce64ff32b57a
syedhazeera <129369062+syedhazeera@users.noreply.github.com>
2023-04-06 15:18:40 +0530

    Add files via upload

---
+++ bitwise&bitfeild/bin_pattern.c
@@ -0,0 +1,23 @@
+//printing the binary pattern of a 16 bit integer.
+#include<stdio.h>
+int bit_pattern(int);
+int main()
+{
+	int a;
+	printf("enter an integer:\n");
+	scanf("%d",&a);
+	bit_pattern(a);
+}
+int bit_pattern(int a)
+{
+	int i,mask;
+	for(i=15;i>=0;i--)
+	{
+		mask=1<<i;
+		if((a&mask)==0)
+			printf("0");
+		else
+			printf("1");
+	}
+	printf("\n");
+}
