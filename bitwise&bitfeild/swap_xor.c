feed14d6bb1ba75493d7909e11d007b9ae62cd8e
syedhazeera <129369062+syedhazeera@users.noreply.github.com>
2023-04-06 20:22:23 +0530

    Add files via upload

---
+++ bitwise&bitfeild/swap_xor.c
@@ -0,0 +1,13 @@
+//program to swap the values of 2 variables using bitwise Xor
+#include<stdio.h>
+int main()
+{
+	int x,y;
+	printf("enter the values of x and y:\n"); 
+	scanf("%d %d",&x,&y);
+	printf("x=%d y=%d\n",x,y);
+	x = x^y;
+	y = x^y;
+	x = x^y;
+	printf("x=%d y=%d\n",x,y);
+}
