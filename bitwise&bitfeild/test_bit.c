abe6af1623c5868dd3b41396461f2bea4cb6bf69
syedhazeera <129369062+syedhazeera@users.noreply.github.com>
2023-04-06 14:55:52 +0530

    Update test_bit.c

--- bitwise&bitfeild/test_bit.c
+++ bitwise&bitfeild/test_bit.c
@@ -13,4 +13,12 @@ int main()
 		bit =1;
 	printf("the bit at position %d is %d\n",bitposition,bit);
 }
-		
+/*output:
+$ ./test_bit
+enter an integer:
+ and bit position:
+5
+2
+a=5     the bit at position 2 is 1
+
+*/
