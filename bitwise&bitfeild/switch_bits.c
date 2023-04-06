01295761d6e1f06b69fe48a15d9aabb34dc0a792
syedhazeera <129369062+syedhazeera@users.noreply.github.com>
2023-04-06 15:06:23 +0530

    Add files via upload

---
+++ bitwise&bitfeild/switch_bits.c
@@ -0,0 +1,15 @@
+//program to switch on all the bits in an integer variable
+#include<stdio.h>
+int main()
+{
+	int a,mask,i;
+	printf("enter the value of integer:\n");
+	scanf("%d",&a);
+	printf("%d\t",a);
+	for(i=0;i<=15;i++)
+	{
+		mask = 1<<i;
+		a = a|mask;
+	}
+	printf("%d\t",a);
+}
