4316e48e1178d9322263b220cf95ddc5e113ee32
syedhazeera <129369062+syedhazeera@users.noreply.github.com>
2023-04-07 09:55:25 +0530

    Add files via upload

---
+++ bitwise&bitfeild/2com.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+	int num,i,mask;
+	printf("enter a number:\n");
+	scanf("%d",&num);
+	printf("twos complement is :");
+	for(i=0;i<=15;i++)
+	{
+		mask = 1<<i;
+		if((num&mask)!=0)
+		break;
+	}
+	for(i=i+1;i<=15;i++)
+	{
+		mask=1<<i;
+		num = num^mask;
+	}
+	printf("twos complement is :%d\n",num);
+return 0;
+}
