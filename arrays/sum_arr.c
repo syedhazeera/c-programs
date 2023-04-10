6c5672c540f805901164513904672d3f68da55b4
syedhazeera <129369062+syedhazeera@users.noreply.github.com>
2023-04-10 13:11:56 +0530

    Update sum_arr.c

--- arrays/sum_arr.c
+++ arrays/sum_arr.c
@@ -10,3 +10,18 @@ int main()
 	}
 	printf("sum = %d\n",sum);
 }
+/*output:
+$ ./sum_arr
+enter the elements of array arr[0] : 1
+enter the elements of array arr[1] : 2
+enter the elements of array arr[2] : 3
+enter the elements of array arr[3] : 4
+enter the elements of array arr[4] : 5
+enter the elements of array arr[5] : 6
+enter the elements of array arr[6] : 7
+enter the elements of array arr[7] : 8
+enter the elements of array arr[8] : 9
+enter the elements of array arr[9] : 10
+sum = 55
+
+*/
