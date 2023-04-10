14f19040719e8b3895e86f3c3ab4c70ca67971c0
syedhazeera <129369062+syedhazeera@users.noreply.github.com>
2023-04-10 13:09:20 +0530

    Update search.c

--- arrays/search.c
+++ arrays/search.c
@@ -1,18 +1,24 @@
 #include<stdio.h>
 int main()
 {
-	int i,item,arr[10]={12,3,5,6,7,8,9,23,21,34};
+	int i,item,arr[10]={12,3,5,6,7,8,9,23,21,34},flag =0;
 	printf("enter the element to be searched:");
 	scanf("%d",&item);
 	for(i=0;i<10;i++)
 	{
-		if(item = arr[i])
+		if(item == arr[i])
 		{
-		printf("%d is found at %d position:",item,i+1);
+		printf(" search sucessful %d is found at %d position:",item,i);
 		break;
 		}
 	}
-	if(i==10)
-		printf("item %d not found in array",item);
+	if(flag = 0)
+		printf("search unsucessful");
 return 0;
 }
+/*output:
+$ ./search
+enter the element to be searched:6
+ search sucessful 6 is found at 3 position:
+
+*/
