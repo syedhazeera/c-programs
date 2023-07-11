2c043eef83c3d7058b67addce85ca181649c3273
syedhazeera <129369062+syedhazeera@users.noreply.github.com>
2023-07-11 12:56:55 +0530

    Add files via upload

---
+++ arrays/ascending_arr.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+int i,j,temp,arr[]={90,34,67,85,21};
+for(i=0;i<5;i++)
+{
+    for(j=0;j<4;j++)
+    {
+        if(arr[j+1]<arr[j])
+        {
+            temp = arr[j];
+            arr[j] = arr[j+1];
+            arr[j+1] = temp;
+        }
+    }
+}
+for(i=0;i<5;i++)
+printf("sorted array is %d\n",arr[i]);
+}
+
+/*output:
+sorted array is 21
+sorted array is 34
+sorted array is 67
+sorted array is 85
+sorted array is 90
+*/
