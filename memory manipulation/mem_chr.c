b95f0f60b34d695f6c376ef4deb48556884a596a
syedhazeera <129369062+syedhazeera@users.noreply.github.com>
2023-07-13 14:40:12 +0530

    Add files via upload

---
+++ memory manipulation/mem_chr.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+int main(int argc, char *argv[])
+{
+	int arr1[] = {3,4};
+	int arr2[] = {1,2};
+	if(memchr(arr1,0,2 * sizeof(int)) != NULL )
+	{
+		printf("found the byte\n");
+	}
+	else
+	{
+		printf("did not find the byte \n");
+	}
+	return 0;
+return 0;
+}
+
+/*output:
+found the byte
+*/
